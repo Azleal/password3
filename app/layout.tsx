582dfb800391e4998ed5a1b6da30c0a222ba45e0
刘南方 <lnf8687@gmail.com>
2024-05-18 13:55:05 +0800

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import '@rainbow-me/rainbowkit/styles.css';
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from './providers';
-
+import Image from "next/image";
+import style from "./layout.module.css";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,11 +21,29 @@ export default function RootLayout({
     <html lang="en">
         <body className={`min-h-screen flex flex-col`}>
          <Providers>
-            <Header />
-            <main>
-              {children}
-            </main>
+            <div className={style.main_layout}>
+                <Header />
+                {children}
+                <div className={style.main_bg}>
+                    <Image
+                        width={184}
+                        height={193}
+                        className={style.image_1}
+                        alt="image_1"
+                        src="/bg/bg1.png"
+                        />
+                    <Image
+                    className={style.image_2}
+                        width={152}
+                        height={270}
+                        alt="image_2"
+                        src="/bg/bg2.png"
+                    />
+                </div>
+            </div>
+ 
           </Providers>
+
         </body>
     </html>
   );
