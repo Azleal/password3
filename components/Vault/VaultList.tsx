32c25817210f8f8ad370d80a8e862f242868a1fc
Azleal <liyao.mr@gmail.com>
2024-05-18 08:38:11 +0800

    vault list style, add dependency

--- components/Vault/VaultList.tsx
+++ components/Vault/VaultList.tsx
@@ -1,48 +1,54 @@
+import { Button } from "antd";
 import Image from "next/image";
 import { VaultType } from "../Contract/Password3Contract";
-import './vaultList.css';
+import "./vaultList.css";
 
-export default function VaultList({vaults}: {vaults: VaultType[]}) {
+export default function VaultList({ vaults }: { vaults: VaultType[] }) {
   return (
     <div className="page flex-col">
       <div className="box_1 flex-col">
         <div className="block_3 flex-row">
-        {
-        vaults.map((e, i) => {
-          return (
-            <div key={i} className="section_1 flex-col">
-              <Image
-              className="image_1"
-              alt="image_1"
-              width={74}
-              height={68}
-              src='/assets/vault.png'
-            />
-            <span className="text_4">{e.title}</span>
-            <span className="text_5">快来添加你的第一条项目吧</span>
-            <div className="text-wrapper_2 flex-col">
-              <span className="text_6">添加一个item</span>
-            </div>
-            </div>
-          )
-        })
-      }
+          {vaults.map((e, i) => {
+            return (
+              <div key={i} className="section_1 flex flex-col items-center">
+                <div className="flex flex-row justify-center items-end h-28">
+                  <Image
+                    className="image_1"
+                    alt="image_1"
+                    width={74}
+                    height={68}
+                    src="/assets/vault.png"
+                  />
+                </div>
+                <span className="text_4">{e.title}</span>
+                <span className="text_5">{e.entrypoint ? '快来添加你的第一条项目吧' : '请先设置口令'}</span>
+                {
+                  e.entrypoint ? (<Button type="primary" className="text-wrapper_2 flex-col justify-center"><span className="text_6">进入Vault</span></Button>): (
+                <Button type="primary" className="text-wrapper_2 flex-col justify-center"><span className="text_6">开始设置</span></Button>)
+                }
+                
+                
+              </div>
+            );
+          })}
         </div>
         <div className="image-wrapper_1 flex-row justify-between">
-          <img
+          <Image
+            alt="image_2"
             className="image_2"
-            src={
-              "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPngda2a08c12ad540582bd8f5352ddb6c4129cf3dd30b28265a1018ab8544bbffff"
-            }
+            width={184}
+            height={193}
+            src="/bg/bg1.png"
           />
-          <img
+          <Image
+            alt="image_3"
             className="image_3"
-            src={
-              "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPngd554d9e1f11622c1b467f22e1c9b4c553e05783159d1ebfbc5cd83afd599b6f1"
-            }
+            width={154}
+            height={154}
+            src="/bg/bg5.png"
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
