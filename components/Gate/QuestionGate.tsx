a1fb824823b4ce44b5c2e6ca088b94865987665c
Azleal <liyao.mr@gmail.com>
2024-05-18 13:53:34 +0800

    scroll address, question gate

--- components/Gate/QuestionGate.tsx
+++ components/Gate/QuestionGate.tsx
@@ -1,15 +1,52 @@
 'use client'
 import { Button, Input } from 'antd'
 import Image from 'next/image'
+import { useState } from 'react'
+import { GateProps } from './GateSetup'
 import style from './questionGate.module.css'
 
 
-export type QuestionGateDataType = {
-
+export type QuestionGateDataItemType = {
+  question: string,
+  answer: string
 }
 
 
-export default function QuestionGate() {
+
+
+export default function QuestionGate(props: GateProps) {
+
+  const [items, setItems] = useState<QuestionGateDataItemType[]>([{question: '', answer: ''}])
+
+  function addNewItem(){
+    const newItem = {question: '', answer: ''}
+    setItems(prevItems => [...prevItems, newItem])
+  }
+
+  /**
+   * questions不为空,
+   * answers不为空
+   */
+  function checkGateData(): Boolean{
+    
+
+    return false
+  }
+
+  async function setNext(){
+    if(!checkGateData()){
+      return
+    }
+    
+  }
+
+  async function setComplete(){
+    if(!checkGateData()){
+      return
+    }
+    
+  }
+  
   return (
     <div className={style.page}>
       <div className={style.section_1 }>
@@ -38,26 +75,34 @@ export default function QuestionGate() {
         </div>
         <div className={style.group_3 }>
           <span className={style.text_5}>（最多输入十个问答）</span>
-          <span className={style.text_6}>问题1</span>
-          <div className={style.text_wrapper_2 }>
-            <Input placeholder='请输入问题'></Input>
-          </div>
-          <div className={style.text_wrapper_3 }>
-            <Input placeholder='请输入答案'></Input>
-          </div>
+          
+          {items.map((e,i) => {
+            return (
+              <div key={i}>
+                <span className={style.text_6}>问题{i+1}</span>
+                <div className={style.text_wrapper_2 }>
+                  <Input placeholder='请输入问题'></Input>
+                </div>
+                <div className={style.text_wrapper_3 }>
+                  <Input placeholder='请输入答案'></Input>
+                </div>
+              </div>
+            )
+          })}
           <Image
             className={style.label_2}
             alt="label2"
             width={36}
             height={36}
+            onClick={addNewItem}
             src='/assets/plus.png'
           />
           <div className={style.box_2}>
             <div className={style.text_wrapper_4}>
-              <Button type='primary' className=' h-full'>设置下一道门</Button>
+              <Button type='primary' className='h-full' onClick={setNext} disabled={!checkGateData()}>设置下一道门</Button>
             </div>
             <div className={style.text_wrapper_5}>
-              <Button type='primary' className=' h-full'>完成设置</Button>
+              <Button type='primary' className=' h-full' onClick={setComplete} disabled={!checkGateData()}>完成设置</Button>
             </div>
           </div>
         </div>
