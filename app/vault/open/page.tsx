27485974f1825dab007dde5dad5eb26abff288f3
Azleal <liyao.mr@gmail.com>
2024-05-18 17:22:30 +0800

    get search param

--- app/vault/open/page.tsx
+++ app/vault/open/page.tsx
@@ -1,12 +1,25 @@
 'use client'
 
 import ReadGate from "@/components/Gate/ReadGate"
+import { useSearchParams } from "next/navigation"
+import { Suspense } from "react"
+
+
+function OpenVaultGate(){
+  const searchParams = useSearchParams()
+  const _vaualtId = searchParams.get("vault")
+  const vaultId = _vaualtId ? Number(_vaualtId) : null;
+  if(vaultId === null){
+    return <>invalid vault id</>
+  }
+  return (<ReadGate vaultId={vaultId} to="/vault/items"/>)
+}
 
 export default function OpenVault() {
   
   return (
-    <>
-      <ReadGate vaultId={1} to="/vault/items"/>
-    </>
+    <Suspense>
+      <OpenVaultGate/>
+    </Suspense>
   )
 }
